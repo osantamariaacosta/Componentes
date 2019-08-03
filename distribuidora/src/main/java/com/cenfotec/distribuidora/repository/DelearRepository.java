6a72b1d2ce4a47adb625ca76ea2561cb5047cc87
Olman <osantamariaa@ucenfotec.ac.cr>
2019-08-02 18:43:44 -0600

    reemplazo de codigo

--- distribuidora/src/main/java/com/cenfotec/distribuidora/repository/DelearRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cenfotec.distribuidora.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cenfotec.distribuidora.domain.Delear;
-
-public interface DelearRepository extends JpaRepository<Delear, Long> {
-
-}
